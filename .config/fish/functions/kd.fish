d47e191012e823ee9513e07bff015d5a886b3bf0
Micky Clerkin-Oliver <mclerkinoliver@confluent.io>
2023-06-16 14:39:03 +0100

    remove private func

--- .config/fish/functions/kd.fish
+++
@@ -1,4 +0,0 @@
-function kd --wraps='KUBECONFIG=~/.kube/ccloud-config/devel/kubeconfig kubectl' --description 'alias kd=KUBECONFIG=~/.kube/ccloud-config/devel/kubeconfig kubectl'
-  KUBECONFIG=~/.kube/ccloud-config/devel/kubeconfig kubectl $argv
-        
-end
